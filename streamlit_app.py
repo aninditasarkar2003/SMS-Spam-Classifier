60706ad91c7b56853d89e0fa200290d7a2c7a2c6
aninditasarkar678@gmail.com <aninditasarkar678@gmail.com>
2024-12-31 01:25:05 +0530

    won

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,33 +19,10 @@ def transform_text(text):
     # Remove non-alphanumeric tokens and stopwords
     text = [i for i in text if i.isalnum()]
     text = [i for i in text if i not in stop_words]
-
-    # Apply stemming
     text = [ps.stem(i) for i in text]
-
     return " ".join(text)
 
-# Step 1: Load the trained model and vectorizer
-with open('model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
-
-with open('vectorizer.pkl', 'rb') as vectorizer_file:
-    tfidf = pickle.load(vectorizer_file)
-
-# Example SMS input
+# Example usage
 input_sms = "Congratulations! You've won a free gift!"
-
-# Step 2: Preprocess the input text
 transformed_sms = transform_text(input_sms)
-
-# Step 3: Vectorize the text using the loaded vectorizer
-vector_input = tfidf.transform([transformed_sms])
-
-# Step 4: Make predictions using the loaded model
-result = model.predict(vector_input)[0]
-
-# Step 5: Display the result
-if result == 1:
-    print("Spam")
-else:
-    print("Not Spam")
+print(transformed_sms)
