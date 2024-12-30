3ee7122e06dfe0adf9a5eb1500cbe755e1bdd4e1
aninditasarkar678@gmail.com <aninditasarkar678@gmail.com>
2024-12-31 01:04:15 +0530

    done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,28 @@
+# Import necessary libraries
 import streamlit as st
 import pickle
-from nltk.tokenize import word_tokenize
+import string
+import nltk
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 import os
-import nltk
 
-# Function to preprocess text
+# Set NLTK Data Path
+nltk_data_dir = os.path.join(os.getcwd(), 'nltk_data')
+
+# Create the directory if it doesn't exist
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir)
+
+# Add the directory to NLTK data path
+nltk.data.path.append(nltk_data_dir)
+
+# Download necessary NLTK resources (punkt, stopwords)
+nltk.download('punkt', download_dir=nltk_data_dir)
+nltk.download('stopwords', download_dir=nltk_data_dir)
+
+# Define the text preprocessing function
 def transform_text(text):
     ps = PorterStemmer()
     text = text.lower()  # Convert to lowercase
@@ -21,24 +37,20 @@ def transform_text(text):
 
     return " ".join(text)  # Return the processed text as a string
 
-# Set NLTK Data Path and Download Punkt Tokenizer
-nltk_data_dir = os.path.join(os.getcwd(), 'nltk_data')
-if not os.path.exists(nltk_data_dir):
-    os.makedirs(nltk_data_dir)
-nltk.data.path.append(nltk_data_dir)
-nltk.download('punkt', download_dir=nltk_data_dir)
-nltk.download('stopwords', download_dir=nltk_data_dir)
-
-# Load the model and vectorizer
+# Load the saved model and vectorizer
 with open('model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
+
 with open('vectorizer.pkl', 'rb') as vectorizer_file:
     tfidf = pickle.load(vectorizer_file)
 
 # Streamlit UI setup
 st.title("SMS/Email Spam Classifier")
+
+# Text input from user
 input_sms = st.text_area("Enter the message")
 
+# When the Predict button is pressed
 if st.button('Predict'):
     # Preprocess the input
     transformed_sms = transform_text(input_sms)
