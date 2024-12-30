dbe4ce0e7bfb13dd8eb7dc5db44edd9171e2232f
aninditasarkar678@gmail.com <aninditasarkar678@gmail.com>
2024-12-31 00:54:06 +0530

    changes done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,68 +1,18 @@
-# Import necessary libraries
-import streamlit as st
-import pickle
-import string
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
 import os
+import nltk
 
-# Set NLTK Data Path and Download Punkt Tokenizer
+# Path to save NLTK data
 nltk_data_dir = os.path.join(os.getcwd(), 'nltk_data')
 
-# Create the directory if it doesn't exist
+# Create the folder if it doesn't exist
 if not os.path.exists(nltk_data_dir):
     os.makedirs(nltk_data_dir)
 
-# Add the directory to NLTK data path
+# Add the directory to NLTK's search path
 nltk.data.path.append(nltk_data_dir)
 
-# Download the 'punkt' tokenizer (only if it's not already downloaded)
+# Download the 'punkt' tokenizer (for breaking text into words and sentences)
 nltk.download('punkt', download_dir=nltk_data_dir)
 
-# Download stopwords (only if it's not already downloaded)
+# Download the stopwords data (for filtering common words like "the", "is", etc.)
 nltk.download('stopwords', download_dir=nltk_data_dir)
-
-# Preprocessing function
-def transform_text(text):
-    ps = PorterStemmer()
-    text = text.lower()
-    text = word_tokenize(text)
-
-    # Remove non-alphanumeric tokens and stopwords, then apply stemming
-    text = [i for i in text if i.isalnum()]
-    text = [i for i in text if i not in stopwords.words('english')]
-    text = [ps.stem(i) for i in text]
-
-    return " ".join(text)
-
-# Load the saved model and vectorizer
-with open('model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
-
-with open('vectorizer.pkl', 'rb') as vectorizer_file:
-    tfidf = pickle.load(vectorizer_file)
-
-# Streamlit UI setup
-st.title("SMS/Email Spam Classifier")
-
-# Text input from user
-input_sms = st.text_area("Enter the message")
-
-# When the Predict button is pressed
-if st.button('Predict'):
-    # Preprocess the input
-    transformed_sms = transform_text(input_sms)
-
-    # Vectorize the input text
-    vector_input = tfidf.transform([transformed_sms])
-
-    # Make the prediction
-    result = model.predict(vector_input)[0]
-
-    # Display the result
-    if result == 1:
-        st.header("Spam")
-    else:
-        st.header("Not Spam")
